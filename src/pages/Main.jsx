c5089df634f1a55ece704fdfaa8bc24bd3b0c6ed
wowba <leeyuwk54@gmail.com>
2023-10-04 15:03:02 +0900

    미사용 코드 제거

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { Header } from '../components/Header'
 import { MainContentBox } from '../components/MainContentBox'
 import { ReactComponent as LogoIcon } from '../asset/search.svg'
 
